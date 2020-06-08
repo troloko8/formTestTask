0fa3ff0e0c085d4f85646bff5ec21b28e2ded28e
troloko <troloko8@gmail.com>
2020-06-08 21:32:19 +0300

    Updates

---
+++ precache-manifest.8f364f2e07c76679a50f59de9a920cb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127647c1c178990d4b78f6bcc7359708",
+    "url": "/formTestTask/index.html"
+  },
+  {
+    "revision": "a4f7791ea851b8fff411",
+    "url": "/formTestTask/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "2053d15cbe1b4de3b882",
+    "url": "/formTestTask/static/css/main.a0f0ba2e.chunk.css"
+  },
+  {
+    "revision": "a4f7791ea851b8fff411",
+    "url": "/formTestTask/static/js/2.70bf6dfd.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/formTestTask/static/js/2.70bf6dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2053d15cbe1b4de3b882",
+    "url": "/formTestTask/static/js/main.7de821ac.chunk.js"
+  },
+  {
+    "revision": "84c851ce558a9234cbd4",
+    "url": "/formTestTask/static/js/runtime-main.57694847.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/formTestTask/static/media/logo.5d5d9eef.svg"
+  }
+]);
